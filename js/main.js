c840f9b04741449d60d22cd4ed0ce5f2a8fe80f6
esalaz415 <esalaz415@gmail.com>
2018-02-12 02:17:10 -0800

    game working cpu finally moving design clean up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
+
   $(".parallax.faster").css("transform","translateY(" +  (scroll/3)  + "px)");
   $(".parallax.slower").css("transform","translateY(" +  (scroll/2.5)  + "px)");
   $(".parallax.medium").css("transform","translateY(" +  (scroll/3.8)  + "px)");
